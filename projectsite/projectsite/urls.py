57ea7c3336f0fe32e582616549ed2b5d43215d0d
n3mo1101 <rd.cruz1101@gmail.com>
2025-09-09 16:44:44 +0800

    Organization delete view done

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView
 from studentorg import views
+from studentorg.views import ( 
+    HomePageView, 
+    OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,4 +30,5 @@ urlpatterns = [
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>',OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
 ]
